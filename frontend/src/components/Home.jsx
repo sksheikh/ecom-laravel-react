5e92ea4a7bbe938f87bd0f4a65d5facfd15610cb
Skuddin <sheikh@nextgenitltd.com>
2025-02-27 08:38:09 +0600

    resolved frontend style.scss issue

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
-import Logo from '../assets/images/logo.png'
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Logo from '../assets/images/logo.png';
+// import Swiper styles
+import 'swiper/css';
+import SliderImgOne from '../assets/images/banner-1.jpg';
+import SliderImgTwo from '../assets/images/banner-2.jpg';
+
 
 const Home = () => {
     return (
@@ -32,7 +38,7 @@ const Home = () => {
                                 </a>
 
                                 <a href="" className='nav-link ms-3'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="28" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16"><path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"></path></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="28" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16"><path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"></path></svg>
                                 </a>
                             </div>
 
@@ -41,6 +47,30 @@ const Home = () => {
                 </div>
 
             </header>
+
+            {/***** Swiper Slider Main **********/}
+            <div className="hero-section">
+                <Swiper
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    breakpoints={{
+                        1024: {
+                            slidesPerView: 1,
+                            spaceBetween: 0,
+                        }
+                    }}
+                >
+                    <SwiperSlide>
+                        <div className="content" style={{ backgroundImage: `url(${SliderImgOne})` }}>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="content" style={{ backgroundImage: `url(${SliderImgTwo})` }}>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+            {/***** Swiper Slider ***********/}
         </>
     )
 }
