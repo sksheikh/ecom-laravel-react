48dba462e6bbd7dee836f5d8d2ba31891b7a4dd7
Skuddin <sheikh@nextgenitltd.com>
2025-03-26 11:01:09 +0600

    category show by id api

--- backend/app/Http/Controllers/admin/CategoryController.php
+++ backend/app/Http/Controllers/admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -14,5 +15,46 @@ class CategoryController extends Controller
             'status' => 200,
             'data' => $categories
         ]);
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ],400);
+        }
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->status = $request->status;
+        $category->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category added successfully.',
+            'data' => $category
+        ]);
+    }
+
+    public function show($id){
+        $category = Category::find($id);
+
+        if($category == null){
+            return response()->json([
+                'status' => 404,
+                'data' => 'Category not found'
+            ],404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $category
+        ], 200); 
     }
 }
