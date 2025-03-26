dc196c6c34be353f43f3396612bce35108bcc978
Skuddin <sheikh@nextgenitltd.com>
2025-03-26 11:15:37 +0600

    category updated api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,6 +15,7 @@ Route::post('admin/login', [AuthController::class, 'authenticate']);
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('categories', [CategoryController::class, 'index']);
     Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::post('categories/{id}', [CategoryController::class, 'update']);
     Route::post('categories/store', [CategoryController::class, 'store']);
 
 });
