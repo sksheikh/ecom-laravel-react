a2db036eba6ea9d3f88991b2f359e83644b95c7e
Skuddin <sheikh@nextgenitltd.com>
2025-04-08 21:36:52 +0600

    product filter api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,14 +13,20 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('admin/login', [AuthController::class, 'authenticate']);
-Route::get('get-latest-products',[FrontProductController::class, 'latestProducts']);
-Route::get('get-featured-products',[FrontProductController::class, 'featuredProducts']);
+Route::get('get-latest-products', [FrontProductController::class, 'latestProducts']);
+Route::get('get-featured-products', [FrontProductController::class, 'featuredProducts']);
+Route::get('get-categories', [FrontProductController::class, 'getCategories']);
+Route::get('get-brands', [FrontProductController::class, 'getBrands']);
+Route::get('get-products', [FrontProductController::class, 'getProducts']);
+
+
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::group(['middleware' => 'auth:sanctum'], function(){
+Route::group(['middleware' => 'auth:sanctum'], function () {
     // Route::get('categories', [CategoryController::class, 'index']);
     // Route::get('categories/{id}', [CategoryController::class, 'show']);
     // Route::post('categories/{id}', [CategoryController::class, 'update']);
@@ -33,7 +39,6 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::apiResource('products', ProductController::class);
     Route::post('temp-images', [TempController::class, 'store']);
     Route::post('save-product-image', [ProductController::class, 'saveProductImage']);
-    Route::get('change-product-default-image',[ProductController::class, 'updateDefaultImage']);
+    Route::get('change-product-default-image', [ProductController::class, 'updateDefaultImage']);
     Route::delete('product-image-delete/{id}', [ProductController::class, 'deleteProductImage']);
-
 });
